5598e3f77173cf69aadd5c3f819e929104e8f723
TeichlerV <117635756+TeichlerV@users.noreply.github.com>
2023-03-07 13:50:20 +0100

    Added new npm icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,9 +14,9 @@ export default function WeatherInfo(props) {
       </div>
       <div className="row">
         <div className="col-6 text-center">
-          <span className="icon">
-            <img src={props.data.icon} alt={props.data.description} />
-          </span>
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+          </div>
 
           <span className="temperature">
             {Math.round(props.data.temperature)}{" "}
