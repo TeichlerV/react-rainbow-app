6ae9929a4e8171ed71821f82496804ab4dba17bd
TeichlerV <117635756+TeichlerV@users.noreply.github.com>
2023-03-14 11:14:17 +0100

    Added Forecast for 1 day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
 import axios from "axios";
+import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecastData, setForecastData] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecastData(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = `c8735bb7e8e2f8d8a38c7501f3cd47d3`;
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="Forecast-day">Mon</div>
-          <WeatherIcon code="01d" size={35} color="#e5d602" />
-          <div className="forecast-temperature">
-            <span className="Forecast-temperature-max">10°</span>
-            <span className="Forecast-temperature-min">5°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <ForecastDay data={forecastData[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `c8735bb7e8e2f8d8a38c7501f3cd47d3`;
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
